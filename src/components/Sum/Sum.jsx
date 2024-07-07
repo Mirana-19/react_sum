4e1bf979d88040321320952354914790a6fa9b3d
Marta Żak <martysik19@gmail.com>
2024-07-07 13:01:39 +0200

    task done

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,24 @@
-// export const Sum = () => ();
+export const Sum = ({ a, b, children, className }) => (
+  <p className={className || 'regular'}>
+    {children} {a} and {b} is {a + b}
+  </p>
+);
+
+/* import { Component } from 'react';
+
+export class Sum extends Component {
+  state = {
+    a: this.props.a,
+    b: this.props.b,
+  };
+
+  render() {
+    return (
+      <p>
+        Sum of {this.state.a} and {this.state.b} is{' '}
+        {this.state.a + this.state.b}
+      </p>
+    );
+  }
+}
+ */
